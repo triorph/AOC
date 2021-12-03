6a367f61d5b09b659d96037c6910852828f736dc
Michael Walsh <Michael.Walsh@canterbury.ac.nz>
2021-12-04 09:30:08 +1300

    Partial work on refactoring the day3 to use a DiagnosticReport object based on a BinaryValue enum (using PEG again)

--- rust/day3/src/main.rs
+++ rust/day3/src/main.rs
@@ -1,4 +1,101 @@
 extern crate peg;
+#[derive(PartialEq)]
+pub enum BinaryValue {
+    One,
+    Zero,
+}
+
+struct DiagnosticReport {
+    binary_values: Vec<Vec<BinaryValue>>,
+}
+
+impl DiagnosticReport {
+    fn new(lines: std::str::Lines) -> DiagnosticReport {
+        DiagnosticReport {
+            binary_values: lines
+                .map(|line| binary_parser::parse(line))
+                .map(Result::unwrap)
+                .collect(),
+        }
+    }
+    fn find_bits_matching_value(
+        self: &DiagnosticReport,
+        bit_index: usize,
+        value_to_match: BinaryValue,
+    ) -> (usize, usize) {
+        let num_matching: usize = self
+            .binary_values
+            .iter()
+            .map(|line| (line[bit_index] == value_to_match) as usize)
+            .sum();
+        let num_not_matching: usize = self
+            .binary_values
+            .iter()
+            .map(|line| (line[bit_index] != value_to_match) as usize)
+            .sum();
+        (num_not_matching, num_matching)
+    }
+    fn filter_by_char_at_bit(
+        self: &mut DiagnosticReport,
+        bit_index: usize,
+        value_to_match: &BinaryValue,
+    ) {
+        if self.binary_values.len() > 1 {
+            self.binary_values = self
+                .copy_binary_values()
+                .into_iter()
+                .filter(|line| line[bit_index] == *value_to_match)
+                .collect();
+        };
+    }
+
+    fn copy_binary_values(self: &DiagnosticReport) -> Vec<Vec<BinaryValue>> {
+        self.binary_values
+            .iter()
+            .map(|line| {
+                line.iter()
+                    .map(|value| match value {
+                        BinaryValue::One => BinaryValue::One,
+                        BinaryValue::Zero => BinaryValue::Zero,
+                    })
+                    .collect()
+            })
+            .collect()
+    }
+
+    fn copy(self: &DiagnosticReport) -> DiagnosticReport {
+        DiagnosticReport {
+            binary_values: self.copy_binary_values(),
+        }
+    }
+
+    fn filter_down_most_common(self: &mut DiagnosticReport, oxygen: bool) -> usize {
+        let mut ret: usize = 0;
+        let (value_to_match, value_to_not_match, match_increase, not_match_increase) = match oxygen
+        {
+            true => (BinaryValue::One, BinaryValue::Zero, 1, 0),
+            false => (BinaryValue::Zero, BinaryValue::One, 0, 1),
+        };
+        for bit_index in 0..self.binary_values[0].len() {
+            ret *= 2;
+            if self.binary_values.len() > 1 {
+                let (num_not_match, num_match) =
+                    self.find_bits_matching_value(bit_index, BinaryValue::One);
+                if num_match >= num_not_match {
+                    ret += match_increase;
+                    self.filter_by_char_at_bit(bit_index, &value_to_match);
+                } else {
+                    ret += not_match_increase;
+                    self.filter_by_char_at_bit(bit_index, &value_to_not_match);
+                }
+            } else if self.binary_values[0][bit_index] == BinaryValue::One {
+                ret += 1;
+            }
+        }
+        ret
+    }
+}
+
 fn main() {
     let lines: Vec<&str> = include_str!("../input_data.txt").lines().collect();
     let day_a = calculate_day_a(&lines[..]);
@@ -92,6 +189,13 @@ mod test {
     use crate::calculate_day_a;
     use crate::calculate_day_b;
     use crate::filter_down_most_common;
+    use crate::DiagnosticReport;
+    #[test]
+    fn test_new_diagnostic() {
+        let lines = include_str!("../test_data.txt").lines();
+        let report = DiagnosticReport::new(lines);
+    }
+
     #[test]
     fn test_day_a() {
         let lines: Vec<&str> = include_str!("../test_data.txt").lines().collect();
@@ -118,10 +222,14 @@ mod test {
 }
 
 peg::parser! { grammar binary_parser() for str {
-    rule binary() -> String
-        = n:$(['0'..='1']+) {String::from(n)}
+    rule binary() -> BinaryValue
+        = n:(one() / zero()) { n }
+    rule one() -> BinaryValue
+        = "1" { BinaryValue::One }
+    rule zero() -> BinaryValue
+        = "0" { BinaryValue::Zero }
+    pub rule parse() -> Vec<BinaryValue>
+        = binary:binary() ** "" { binary }
 
-    pub rule parse() -> String
-        = binary:binary() { binary }
 }
 }
