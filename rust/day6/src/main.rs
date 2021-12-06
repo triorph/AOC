1b45f1173f846778b487f95d1f8d0e169df7ab03
Michael Walsh <Michael.Walsh@canterbury.ac.nz>
2021-12-07 10:17:53 +1300

    Create a LanternFishIterator to handle the iteration of the LanternFish population

--- rust/day6/src/main.rs
+++ rust/day6/src/main.rs
@@ -20,33 +20,42 @@ peg::parser! { grammar day6_parser() for str {
 }
 }
 
+struct LanternFishIterator {
+    current_lantern: LanternFishState,
+}
+
+impl Iterator for LanternFishIterator {
+    type Item = LanternFishState;
+    fn next(self: &mut LanternFishIterator) -> Option<Self::Item> {
+        let mut ret = [0; 9];
+        for (i, fish_count) in self.current_lantern.lantern_count_by_age[1..]
+            .iter()
+            .enumerate()
+        {
+            ret[i] = *fish_count;
+        }
+        ret[6] += self.current_lantern.lantern_count_by_age[0];
+        ret[8] = self.current_lantern.lantern_count_by_age[0];
+        self.current_lantern = LanternFishState {
+            lantern_count_by_age: ret,
+        };
+        Some(self.current_lantern.clone())
+    }
+}
+
 impl LanternFishState {
     fn new(vent_layout_input_str: &str) -> LanternFishState {
         day6_parser::parse(vent_layout_input_str).unwrap()
     }
 
-    fn get_next_state(self: &LanternFishState) -> LanternFishState {
-        let mut ret = [0; 9];
-        for (i, fish_count) in self.lantern_count_by_age[1..].iter().enumerate() {
-            ret[i] = *fish_count;
-        }
-        ret[6] += self.lantern_count_by_age[0];
-        ret[8] = self.lantern_count_by_age[0];
-        LanternFishState {
-            lantern_count_by_age: ret,
-        }
-    }
-
     fn get_after_n_iterations(self: &LanternFishState, n: usize) -> LanternFishState {
-        let mut ret = self.clone();
-        for _ in 0..n {
-            ret = ret.get_next_state();
-        }
-        ret
+        self.iter().take(n).last().unwrap()
     }
 
-    fn count(self: &LanternFishState) -> usize {
-        return self.lantern_count_by_age.iter().sum();
+    fn iter(self: &LanternFishState) -> LanternFishIterator {
+        LanternFishIterator {
+            current_lantern: self.clone(),
+        }
     }
 
     fn calculate_day_a(self: &LanternFishState) -> usize {
@@ -56,12 +65,16 @@ impl LanternFishState {
     fn calculate_day_b(self: &LanternFishState) -> usize {
         self.get_after_n_iterations(256).count()
     }
+    fn count(self: &LanternFishState) -> usize {
+        return self.lantern_count_by_age.iter().sum();
+    }
 }
 
 fn main() {
     let lantern_state = LanternFishState::new(include_str!("../input_data.txt"));
     let day_a = lantern_state.calculate_day_a();
     println!("Day a result: {}", day_a);
+    let lantern_state = LanternFishState::new(include_str!("../input_data.txt"));
     let day_b = lantern_state.calculate_day_b();
     println!("Day b result: {}", day_b);
 }
@@ -82,14 +95,23 @@ mod test {
     #[test]
     fn test_get_next_state() {
         let lantern_state = LanternFishState::new(include_str!("../test_data.txt"));
-        let ret = lantern_state.get_next_state();
-        assert_eq!(ret.lantern_count_by_age, [1, 1, 2, 1, 0, 0, 0, 0, 0]);
-        let ret = ret.get_next_state();
-        assert_eq!(ret.lantern_count_by_age, [1, 2, 1, 0, 0, 0, 1, 0, 1]);
-        let ret = ret.get_next_state();
-        assert_eq!(ret.lantern_count_by_age, [2, 1, 0, 0, 0, 1, 1, 1, 1]);
-        let ret = ret.get_next_state();
-        assert_eq!(ret.lantern_count_by_age, [1, 0, 0, 0, 1, 1, 3, 1, 2]);
+        let mut lantern_iter = lantern_state.iter();
+        assert_eq!(
+            lantern_iter.next().unwrap().lantern_count_by_age,
+            [1, 1, 2, 1, 0, 0, 0, 0, 0]
+        );
+        assert_eq!(
+            lantern_iter.next().unwrap().lantern_count_by_age,
+            [1, 2, 1, 0, 0, 0, 1, 0, 1]
+        );
+        assert_eq!(
+            lantern_iter.next().unwrap().lantern_count_by_age,
+            [2, 1, 0, 0, 0, 1, 1, 1, 1]
+        );
+        assert_eq!(
+            lantern_iter.next().unwrap().lantern_count_by_age,
+            [1, 0, 0, 0, 1, 1, 3, 1, 2]
+        );
     }
 
     #[test]
