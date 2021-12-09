6d38c1018c4bca8adeb337cbdcd95868a5695e3d
Michael Walsh <Michael.Walsh@canterbury.ac.nz>
2021-12-10 11:17:56 +1300

    Still not quite working, but breaks down common elements in len(5) and len(6)

--- rust/day8/src/main.rs
+++ rust/day8/src/main.rs
@@ -331,11 +331,76 @@ impl DigitLine {
         })
     }
 
+    fn update_possibilities_for_len_6_digits(
+        self: &DigitLine,
+        possibilities: &mut HashMap<DigitSegment, Vec<DigitSegment>>,
+    ) {
+        let len_6_digits = self
+            .input_digits
+            .clone()
+            .into_iter()
+            .filter(|digit| digit.input_segments.len() == 6)
+            .collect::<Vec<Digit>>();
+        let common_digits =
+            len_6_digits
+                .iter()
+                .fold(DigitSegment::all_segments().to_vec(), |ret, next| {
+                    ret.into_iter()
+                        .filter(|val| next.input_segments.contains(val))
+                        .collect()
+                });
+
+        Digit {
+            input_segments: common_digits,
+            digit_value: DigitValue::Unknown,
+        }
+        .filter_down_possibilities(
+            possibilities,
+            vec![
+                DigitSegment::A,
+                DigitSegment::B,
+                DigitSegment::F,
+                DigitSegment::G,
+            ],
+        )
+    }
+
+    fn update_possibilities_for_len_5_digits(
+        self: &DigitLine,
+        possibilities: &mut HashMap<DigitSegment, Vec<DigitSegment>>,
+    ) {
+        let len_5_digits = self
+            .input_digits
+            .clone()
+            .into_iter()
+            .filter(|digit| digit.input_segments.len() == 5)
+            .collect::<Vec<Digit>>();
+        let common_digits =
+            len_5_digits
+                .iter()
+                .fold(DigitSegment::all_segments().to_vec(), |ret, next| {
+                    ret.into_iter()
+                        .filter(|val| next.input_segments.contains(val))
+                        .collect()
+                });
+
+        Digit {
+            input_segments: common_digits,
+            digit_value: DigitValue::Unknown,
+        }
+        .filter_down_possibilities(
+            possibilities,
+            vec![DigitSegment::A, DigitSegment::D, DigitSegment::G],
+        )
+    }
+
     fn calculate_output(self: &mut DigitLine) -> usize {
         let mut digit_possibilities: HashMap<DigitSegment, Vec<DigitSegment>> = HashMap::new();
         for digit in DigitSegment::all_segments().into_iter() {
             digit_possibilities.insert(digit, DigitSegment::all_segments().to_vec());
         }
+        self.update_possibilities_for_len_5_digits(&mut digit_possibilities);
+
         while self.output_not_found() {
             for digit in &mut self.input_digits[..] {
                 digit.update_possibilities(&mut digit_possibilities)
