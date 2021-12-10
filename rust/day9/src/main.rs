09b500e9066f55ed98ed3df62f81e553d72480bf
Michael Walsh <Michael.Walsh@canterbury.ac.nz>
2021-12-10 16:59:57 +1300

    read file from disk instead of at compile time

--- rust/day9/src/main.rs
+++ rust/day9/src/main.rs
@@ -1,6 +1,8 @@
 extern crate itertools;
 extern crate peg;
 use itertools::Itertools;
+use std::fs::File;
+use std::io::prelude::*;
 
 #[derive(Clone)]
 pub struct SmokeLocations {
@@ -148,10 +150,13 @@ impl<'a> SmokeLocations {
 }
 
 fn main() {
-    let smoke_locations = SmokeLocations::new(include_str!("../input_data.txt"));
+    let mut f = File::open("input_data.txt").expect("Need input_data.txt to exist");
+    let mut buffer = String::new();
+    f.read_to_string(&mut buffer).unwrap();
+    let smoke_locations = SmokeLocations::new(&buffer[..]);
     let day_a = smoke_locations.calculate_day_a();
     println!("Day a result: {}", day_a);
-    let smoke_locations = SmokeLocations::new(include_str!("../input_data.txt"));
+    let smoke_locations = SmokeLocations::new(&buffer[..]);
     let day_b = smoke_locations.calculate_day_b();
     println!("Day b result: {}", day_b);
 }
