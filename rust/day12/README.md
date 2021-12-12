4ac23f910c0a789a72f8fc51764fa3c045f01f23
Michael Walsh <Michael.Walsh@canterbury.ac.nz>
2021-12-12 19:41:31 +1300

    Get day 12 done

--- rust/day12/README.md
+++ rust/day12/README.md
@@ -1,1 +1,10 @@
 ### Day 12
+
+part a:
+Given a set of edges, find all paths from start -> end, following the rule that nodes with lower-case names can only be visited once.
+
+part b:
+Given the same set of edges, how many paths if 1 lower-case name node can be visited twice.
+
+Mostly happy with my current solution, but it seems to be the first one where the time taken has dragged. I wonder what optimisations I can make, preferably
+at a higher level, that fix this. Once rust uses --release it still goes down to 2seconds, so not too bad; but not great either.
--- rust/day12/src/lib.rs
+++ rust/day12/src/lib.rs
@@ -1,16 +1,73 @@
 extern crate peg;
-use itertools::Itertools;
+use std::collections::HashMap;
 
-pub struct Day12Setup {}
+#[derive(Clone, Debug)]
+struct Path(Vec<String>);
+
+pub struct Day12Setup(HashMap<String, Vec<String>>);
 
 peg::parser! { grammar day12_parser() for str {
+    rule lower_node() -> &'input str
+        = node:$(['a'..='z']+) {node }
+    rule upper_node() -> &'input str
+        = node:$(['A'..='Z']+) {node}
     rule number() -> usize
         = n:$(['0'..='9']) { n.parse().expect(&format!("Was expecting a number string {}", n)[..])}
-    pub rule parse() -> Day12Setup
-        = number:number() "\n" * {
-            Day12Setup {}
+    rule node() -> &'input str
+        = node:(lower_node() / upper_node()) { node }
+    rule edge() -> (&'input str, &'input str)
+        = left:node() "-" right:node() { (left, right) }
+    pub rule parse() -> Vec<(&'input str, &'input str)>
+        = edges:edge() ++ "\n" "\n" * {
+            edges
         }
 }}
+
+impl Path {
+    fn last(self: &Path) -> &String {
+        &self.0[self.0.len() - 1]
+    }
+
+    fn is_finished(self: &Path) -> bool {
+        self.last() == &"end".to_string()
+    }
+
+    fn day_a_can_visit_node(self: &Path, next_node: &str) -> bool {
+        Path::node_is_major(next_node) || self.is_small_cave_unvisited(next_node)
+    }
+
+    fn day_b_can_visit_node(self: &Path, next_node: &str) -> bool {
+        Path::node_is_not_start(next_node)
+            && (Path::node_is_major(next_node)
+                || self.is_small_cave_unvisited(next_node)
+                || self.no_small_caves_visited_twice())
+    }
+
+    fn is_small_cave_unvisited(self: &Path, small_cave: &str) -> bool {
+        !self.0.contains(&small_cave.to_string())
+    }
+
+    fn no_small_caves_visited_twice(self: &Path) -> bool {
+        for small_node in self.0.iter().filter(|node| !Path::node_is_major(node)) {
+            if self.0.iter().filter(|node| node == &small_node).count() > 1 {
+                return false;
+            }
+        }
+        true
+    }
+
+    fn node_is_not_start(node: &str) -> bool {
+        node != "start"
+    }
+
+    fn node_is_major(node: &str) -> bool {
+        if let Some(c) = node.chars().next() {
+            ('A'..'Z').contains(&c)
+        } else {
+            false
+        }
+    }
+}
 
 impl Day12Setup {
     /// Generates a new Day12Setup object to be calculated upon
@@ -20,17 +77,95 @@ impl Day12Setup {
     /// Returns: A new Day12Setup object, with methods `calculate_day_a` and `calculate_day_b`
     /// available
     pub fn new(input_str: &str) -> Day12Setup {
-        day12_parser::parse(input_str).unwrap()
+        let edges = day12_parser::parse(input_str).unwrap();
+        let mut hashmap = HashMap::<String, Vec<String>>::new();
+        for (left, right) in edges.into_iter() {
+            let left_string = String::from(left);
+            let right_string = String::from(right);
+            let left_entry = hashmap.entry(left_string.clone()).or_insert_with(Vec::new);
+            (*left_entry).push(right_string.clone());
+            let right_entry = hashmap.entry(right_string).or_insert_with(Vec::new);
+            (*right_entry).push(left_string);
+        }
+        Day12Setup(hashmap)
+    }
+
+    fn get_available_next_nodes_part_a(self: &Day12Setup, path: &Path) -> Vec<String> {
+        self.0
+            .get(path.last())
+            .unwrap()
+            .clone()
+            .into_iter()
+            .filter(|node| path.day_a_can_visit_node(node))
+            .collect()
+    }
+
+    fn get_available_next_nodes_part_b(self: &Day12Setup, path: &Path) -> Vec<String> {
+        self.0
+            .get(path.last())
+            .unwrap()
+            .clone()
+            .into_iter()
+            .filter(|node| path.day_b_can_visit_node(node))
+            .collect()
+    }
+
+    fn build_paths_to_goal_part_a(self: &Day12Setup) -> Vec<Path> {
+        let mut current = vec![Path(vec!["start".to_string()])];
+        loop {
+            let mut next = vec![];
+            for path in current.into_iter() {
+                if path.is_finished() {
+                    next.push(path);
+                } else {
+                    let neighbours = self.get_available_next_nodes_part_a(&path);
+                    for neighbour in neighbours.into_iter() {
+                        let mut path_2 = path.clone();
+                        path_2.0.push(neighbour);
+                        next.push(path_2);
+                    }
+                }
+            }
+            current = next;
+            if current.iter().filter(|path| path.is_finished()).count() == current.len() {
+                break;
+            }
+        }
+        current
+    }
+
+    fn build_paths_to_goal_part_b(self: &Day12Setup) -> Vec<Path> {
+        let mut current = vec![Path(vec!["start".to_string()])];
+        loop {
+            let mut next = vec![];
+            for path in current.into_iter() {
+                if path.is_finished() {
+                    next.push(path);
+                } else {
+                    let neighbours = self.get_available_next_nodes_part_b(&path);
+                    for neighbour in neighbours.into_iter() {
+                        let mut path_2 = path.clone();
+                        path_2.0.push(neighbour);
+                        next.push(path_2);
+                    }
+                }
+            }
+            current = next;
+            if current.iter().filter(|path| path.is_finished()).count() == current.len() {
+                break;
+            }
+        }
+        current
     }
 
     /// Calculate the part a response
     pub fn calculate_day_a(self: &Day12Setup) -> usize {
-        0
+        self.build_paths_to_goal_part_a().len()
     }
 
     /// Calculate the part b response
     pub fn calculate_day_b(self: &Day12Setup) -> usize {
-        0
+        self.build_paths_to_goal_part_b().len()
     }
 }
 
@@ -40,18 +175,26 @@ mod test {
 
     #[test]
     fn test_parse() {
-        let _octo_setup = Day12Setup::new(include_str!("../test_data.txt"));
+        let _day12_setup = Day12Setup::new(include_str!("../test_data.txt"));
     }
 
     #[test]
     fn test_day_a() {
         let day12_setup = Day12Setup::new(include_str!("../test_data.txt"));
-        assert_eq!(day12_setup.calculate_day_a(), 0);
+        assert_eq!(day12_setup.calculate_day_a(), 10);
+        let day12_setup = Day12Setup::new(include_str!("../test_data_2.txt"));
+        assert_eq!(day12_setup.calculate_day_a(), 19);
+        let day12_setup = Day12Setup::new(include_str!("../test_data_3.txt"));
+        assert_eq!(day12_setup.calculate_day_a(), 226);
     }
 
     #[test]
     fn test_day_b() {
         let day12_setup = Day12Setup::new(include_str!("../test_data.txt"));
-        assert_eq!(day12_setup.calculate_day_b(), 0);
+        assert_eq!(day12_setup.calculate_day_b(), 36);
+        let day12_setup = Day12Setup::new(include_str!("../test_data_2.txt"));
+        assert_eq!(day12_setup.calculate_day_b(), 103);
+        let day12_setup = Day12Setup::new(include_str!("../test_data_3.txt"));
+        assert_eq!(day12_setup.calculate_day_b(), 3509);
     }
 }
