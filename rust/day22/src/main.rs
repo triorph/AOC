85f06fb4ba08f6a09b3861c749812bbac3acbcc5
Michael Walsh <Michael.Walsh@canterbury.ac.nz>
2022-01-03 20:46:26 +1300

    Refactor day22 some more.

--- rust/day22/src/main.rs
+++ rust/day22/src/main.rs
@@ -1,4 +1,4 @@
-use day22::Day22Setup;
+use day22::VolumeStepBuilder;
 use std::fs::File;
 use std::io::prelude::*;
 
@@ -6,10 +6,10 @@ fn main() {
     let mut f = File::open("input_data.txt").expect("Need input_data.txt to exist");
     let mut input_str: String = String::new();
     f.read_to_string(&mut input_str).unwrap();
-    let mut day22_setup = Day22Setup::new(&input_str[..]);
+    let mut day22_setup = VolumeStepBuilder::new(&input_str[..]);
     let day_a = day22_setup.calculate_day_a();
     println!("Day a result: {}", day_a);
-    let mut day22_setup = Day22Setup::new(&input_str[..]);
+    let mut day22_setup = VolumeStepBuilder::new(&input_str[..]);
     let day_b = day22_setup.calculate_day_b();
     println!("Day b result: {}", day_b);
 }
--- rust/day22/src/parser.rs
+++ rust/day22/src/parser.rs
@@ -1,6 +1,7 @@
+extern crate peg;
 use crate::cuboid::Cuboid;
 use crate::volumestep::VolumeStep;
-use crate::Day22Setup;
+use crate::VolumeStepBuilder;
 
 peg::parser! { grammar day22_parser() for str {
     rule positive_number() -> isize
@@ -19,20 +20,20 @@ peg::parser! { grammar day22_parser() for str {
         = volume_type:volume_type() " x=" min_x:number() ".." max_x:number() ",y=" min_y:number() ".." max_y:number() ",z=" min_z:number() ".." max_z:number() {
             VolumeStep{ volume_type, block: Cuboid::new( min_x, max_x, min_y, max_y, min_z, max_z ) }
         }
-    pub rule parse() -> Day22Setup
+    pub rule parse() -> VolumeStepBuilder
         = volume_steps:volume_step() ++ "\n" "\n" * {
-            Day22Setup { volume_steps }
+            VolumeStepBuilder { volume_steps }
         }
 }}
 
-impl Day22Setup {
+impl VolumeStepBuilder {
     /// Generates a new Day22Setup object to be calculated upon
     ///
     /// Inputs: the full string contents of the input data set.
     ///
     /// Returns: A new Day22Setup object, with methods `calculate_day_a` and `calculate_day_b`
     /// available
-    pub fn new(input_str: &str) -> Day22Setup {
+    pub fn new(input_str: &str) -> VolumeStepBuilder {
         day22_parser::parse(input_str).unwrap()
     }
 }
