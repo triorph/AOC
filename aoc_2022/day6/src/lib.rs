21e8f36959889c96a78a7425dbc3402651494fb7
Michael Walsh <mwalsh2@atlassian.com>
2022-12-06 10:08:26 +1300

    Forgot to actually git add day6

---
+++ aoc_2022/day6/src/lib.rs
@@ -0,0 +1,50 @@
+mod parser;
+use crate::parser::parse_data;
+use aoc_helpers::{read_input_file, AOCCalculator, AOCFileOrParseError};
+
+pub struct Day6 {
+    data: (),
+}
+
+impl AOCCalculator<usize> for Day6 {
+    fn new(filename: &str) -> Result<Day6, AOCFileOrParseError> {
+        Ok(Day6 {
+            data: parse_data(&read_input_file(filename)?)?,
+        })
+    }
+
+    fn calculate_day_a(&self) -> usize {
+        0
+    }
+
+    fn calculate_day_b(&self) -> usize {
+        0
+    }
+
+    fn print_results(&self, name: &str) {
+        println!("{}a answer is {:?}", name, self.calculate_day_a());
+        println!("{}b answer is {:?}", name, self.calculate_day_b());
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use pretty_assertions::assert_eq;
+
+    #[test]
+    fn test_calculate_day_a() {
+        let day6 = Day6::new("data/test_data.txt").unwrap();
+        let expected = 0;
+        let actual = day6.calculate_day_a();
+        assert_eq!(expected, actual);
+    }
+
+    #[test]
+    fn test_calculate_day_b() {
+        let day6 = Day6::new("data/test_data.txt").unwrap();
+        let expected = 0;
+        let actual = day6.calculate_day_b();
+        assert_eq!(expected, actual);
+    }
+}
