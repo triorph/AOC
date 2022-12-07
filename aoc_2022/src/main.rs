ed4f1deedc3253a2750d64d3104b22e3ed1ac3f4
Michael Walsh <mwalsh2@atlassian.com>
2022-12-08 10:31:45 +1300

    Setup initial template for day8

--- aoc_2022/src/main.rs
+++ aoc_2022/src/main.rs
@@ -7,7 +7,7 @@ use day4::Day4;
 use day5::Day5;
 use day6::Day6;
 use day7::Day7;
-// use day8::Day8;
+use day8::Day8;
 // use day9::Day9;
 // use day10::Day10;
 // use day11::Day11;
@@ -26,7 +26,9 @@ use day7::Day7;
 // use day24::Day24;
 // use day25::Day25
 
-const ALL_CURRENT_DAYS: [&str; 7] = ["day1", "day2", "day3", "day4", "day5", "day6", "day7"];
+const ALL_CURRENT_DAYS: [&str; 8] = [
+    "day1", "day2", "day3", "day4", "day5", "day6", "day7", "day8",
+];
 
 fn run_day_if_enabled<T>(day: Box<dyn AOCCalculator<T>>, name: &str, allowed_days: &[&str]) {
     if allowed_days.contains(&name) {
@@ -72,7 +74,11 @@ fn main() -> Result<()> {
         "day7",
         &allowed_days,
     );
-    // run_day_if_enabled(Box::new(Day8::new("day8/data/input_data.txt")?), "day8", &allowed_days);
+    run_day_if_enabled(
+        Box::new(Day8::new("day8/data/input_data.txt")?),
+        "day8",
+        &allowed_days,
+    );
     // run_day_if_enabled(Box::new(Day9::new("day9/data/input_data.txt")?), "day9", &allowed_days);
     // run_day_if_enabled(Box::new(Day10::new("day10/data/input_data.txt")?), "day10", &allowed_days);
     // run_day_if_enabled(Box::new(Day11::new("day11/data/input_data.txt")?), "day11", &allowed_days);
