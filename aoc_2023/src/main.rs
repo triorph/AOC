7b65bcca17a08e0ba2380ff433c97f2f4a6c11d6
Michael Walsh <mwalsh2@atlassian.com>
2023-12-07 10:25:01 +1300

    Include day7 from skeleton

--- aoc_2023/src/main.rs
+++ aoc_2023/src/main.rs
@@ -26,9 +26,9 @@ use day6::Day6;
 // use day8::Day8;
 // use day9::Day9;
 
-const ALL_CURRENT_DAYS: [&str; 5] = [
-    "day1", "day2", "day3", "day4",
-    "day5", // "day6", "day7", "day8", "day9", "day10", "day11",
+const ALL_CURRENT_DAYS: [&str; 6] = [
+    "day1", "day2", "day3", "day4", "day5",
+    "day6", // "day7", "day8", "day9", "day10", "day11",
            // "day12", "day13", "day14", "day15", "day16", "day17", "day18", "day19", "day20", "day21",
            // "day22", "day23", "day24", "day25",
 ];
