3def1647b6ec6e411ecf4bcc2070737465360c2a
Michael Walsh <mwalsh2@atlassian.com>
2023-12-11 14:39:11 +1300

    Setup skeleton for day 11

---
+++ aoc_2023/day11/src/lib.rs
@@ -0,0 +1,68 @@
+mod parser;
+use crate::parser::parse_data;
+use aoc_helpers::{read_input_file, AOCCalculator, AOCFileOrParseError};
+
+pub struct Day11 {
+    data: (),
+}
+
+impl AOCCalculator for Day11 {
+    fn new(filename: &str) -> Result<Day11, AOCFileOrParseError> {
+        Ok(Day11 {
+            data: parse_data(&read_input_file(filename)?)?,
+        })
+    }
+
+    fn print_results(&self, name: &str) {
+        println!("{}a answer is {:?}", name, self.calculate_day_a());
+        println!("{}b answer is {:?}", name, self.calculate_day_b());
+    }
+}
+
+impl Day11 {
+    fn calculate_day_a(&self) -> usize {
+        0
+    }
+
+    fn calculate_day_b(&self) -> usize {
+        0
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use pretty_assertions::assert_eq;
+
+    #[test]
+    fn test_calculate_day_a() {
+        let day11 = Day11::new("data/test_data.txt").unwrap();
+        let expected = 0;
+        let actual = day11.calculate_day_a();
+        assert_eq!(expected, actual);
+    }
+
+    #[test]
+    fn test_calculate_day_b() {
+        let day11 = Day11::new("data/test_data.txt").unwrap();
+        let expected = 0;
+        let actual = day11.calculate_day_b();
+        assert_eq!(expected, actual);
+    }
+
+    #[test]
+    fn test_real_input_calculate_day_a() {
+        let day11 = Day11::new("data/input_data.txt").unwrap();
+        let expected = 0;
+        let actual = day11.calculate_day_a();
+        assert_eq!(expected, actual);
+    }
+
+    #[test]
+    fn test_real_input_calculate_day_b() {
+        let day11 = Day11::new("data/input_data.txt").unwrap();
+        let expected = 0;
+        let actual = day11.calculate_day_b();
+        assert_eq!(expected, actual);
+    }
+}
