68614b48aabcce0d64dc751b2696d4a3b9b9d52d
Michael Walsh <mwalsh2@atlassian.com>
2023-12-12 17:40:04 +1300

    Add day12 and bump version numbers in skeleton

---
+++ aoc_2023/day12/src/lib.rs
@@ -0,0 +1,68 @@
+mod parser;
+use crate::parser::parse_data;
+use aoc_helpers::{read_input_file, AOCCalculator, AOCFileOrParseError};
+
+pub struct Day12 {
+    data: (),
+}
+
+impl AOCCalculator for Day12 {
+    fn new(filename: &str) -> Result<Day12, AOCFileOrParseError> {
+        Ok(Day12 {
+            data: parse_data(&read_input_file(filename)?)?,
+        })
+    }
+
+    fn print_results(&self, name: &str) {
+        println!("{}a answer is {:?}", name, self.calculate_day_a());
+        println!("{}b answer is {:?}", name, self.calculate_day_b());
+    }
+}
+
+impl Day12 {
+    fn calculate_day_a(&self) -> usize {
+        0
+    }
+
+    fn calculate_day_b(&self) -> usize {
+        0
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use pretty_assertions::assert_eq;
+
+    #[test]
+    fn test_calculate_day_a() {
+        let day12 = Day12::new("data/test_data.txt").unwrap();
+        let expected = 0;
+        let actual = day12.calculate_day_a();
+        assert_eq!(expected, actual);
+    }
+
+    #[test]
+    fn test_calculate_day_b() {
+        let day12 = Day12::new("data/test_data.txt").unwrap();
+        let expected = 0;
+        let actual = day12.calculate_day_b();
+        assert_eq!(expected, actual);
+    }
+
+    #[test]
+    fn test_real_input_calculate_day_a() {
+        let day12 = Day12::new("data/input_data.txt").unwrap();
+        let expected = 0;
+        let actual = day12.calculate_day_a();
+        assert_eq!(expected, actual);
+    }
+
+    #[test]
+    fn test_real_input_calculate_day_b() {
+        let day12 = Day12::new("data/input_data.txt").unwrap();
+        let expected = 0;
+        let actual = day12.calculate_day_b();
+        assert_eq!(expected, actual);
+    }
+}
