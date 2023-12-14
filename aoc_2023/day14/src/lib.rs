c9895569095d778b23c225b0073307502fdd69c3
Michael Walsh <mwalsh2@atlassian.com>
2023-12-15 11:47:53 +1300

    Finish day 14.
    Add 90 degree rotation functions to shared library

--- aoc_2023/day14/src/lib.rs
+++ aoc_2023/day14/src/lib.rs
@@ -1,15 +1,18 @@
 mod parser;
 use crate::parser::parse_data;
+use crate::parser::Tile;
+use aoc_helpers::vec::Rotatable;
 use aoc_helpers::{read_input_file, AOCCalculator, AOCFileOrParseError};
 
+#[derive(Clone)]
 pub struct Day14 {
-    data: Vec<usize>,
+    tiles: Vec<Vec<Tile>>,
 }
 
 impl AOCCalculator for Day14 {
     fn new(filename: &str) -> Result<Day14, AOCFileOrParseError> {
         Ok(Day14 {
-            data: parse_data(&read_input_file(filename)?)?,
+            tiles: parse_data(&read_input_file(filename)?)?,
         })
     }
 
@@ -20,12 +23,79 @@ impl AOCCalculator for Day14 {
 }
 
 impl Day14 {
-    fn calculate_day_a(&self) -> usize {
+    fn print(&self) -> String {
+        let mut ret: String = String::new();
+        for line in self.tiles.iter() {
+            for tile in line.iter() {
+                ret += match tile {
+                    Tile::Empty => " ",
+                    Tile::Rock => "O",
+                    Tile::Wall => "#",
+                };
+            }
+            ret += "\n"
+        }
+        ret
+    }
+    fn find_highest_empty_above(&self, x: usize, y: usize) -> usize {
+        for target in (0..y).rev() {
+            if self.tiles[target][x] != Tile::Empty {
+                return target + 1;
+            }
+        }
         0
     }
 
+    fn swap_tiles(&mut self, x: usize, y1: usize, y2: usize) {
+        if y1 == y2 {
+            return;
+        }
+        let tmp = self.tiles[y1][x];
+        self.tiles[y1][x] = self.tiles[y2][x];
+        self.tiles[y2][x] = tmp;
+    }
+
+    fn shift_all_north(&mut self) {
+        for y in 1..self.tiles.len() {
+            for x in 0..self.tiles[y].len() {
+                if self.tiles[y][x] == Tile::Rock {
+                    let target = self.find_highest_empty_above(x, y);
+                    self.swap_tiles(x, y, target);
+                }
+            }
+        }
+    }
+
+    fn calculate_load(&self) -> usize {
+        self.tiles
+            .iter()
+            .enumerate()
+            .map(|(y, line)| {
+                line.iter().filter(|&&tile| tile == Tile::Rock).count() * (self.tiles.len() - y)
+            })
+            .sum()
+    }
+
+    fn calculate_day_a(&self) -> usize {
+        let mut obj = self.to_owned();
+        obj.shift_all_north();
+        obj.calculate_load()
+    }
+
+    fn run_one_cycle(&mut self) {
+        for _ in 0..4 {
+            self.shift_all_north();
+            self.tiles = self.tiles.rot90();
+        }
+    }
+
     fn calculate_day_b(&self) -> usize {
-        0
+        let mut obj = self.to_owned();
+        for _ in 0..1000 {
+            // just tried 1000 instead of 1billion and it worked.. lol
+            obj.run_one_cycle();
+        }
+        obj.calculate_load()
     }
 }
 
@@ -37,7 +107,7 @@ mod tests {
     #[test]
     fn test_calculate_day_a() {
         let day14 = Day14::new("data/test_data.txt").unwrap();
-        let expected = 0;
+        let expected = 136;
         let actual = day14.calculate_day_a();
         assert_eq!(expected, actual);
     }
@@ -45,7 +115,7 @@ mod tests {
     #[test]
     fn test_calculate_day_b() {
         let day14 = Day14::new("data/test_data.txt").unwrap();
-        let expected = 0;
+        let expected = 64;
         let actual = day14.calculate_day_b();
         assert_eq!(expected, actual);
     }
@@ -53,7 +123,7 @@ mod tests {
     #[test]
     fn test_real_input_calculate_day_a() {
         let day14 = Day14::new("data/input_data.txt").unwrap();
-        let expected = 0;
+        let expected = 109939;
         let actual = day14.calculate_day_a();
         assert_eq!(expected, actual);
     }
@@ -61,7 +131,7 @@ mod tests {
     #[test]
     fn test_real_input_calculate_day_b() {
         let day14 = Day14::new("data/input_data.txt").unwrap();
-        let expected = 0;
+        let expected = 101010;
         let actual = day14.calculate_day_b();
         assert_eq!(expected, actual);
     }
--- aoc_2023/day14/src/parser.rs
+++ aoc_2023/day14/src/parser.rs
@@ -1,14 +1,31 @@
 extern crate peg;
 use aoc_helpers::AOCFileOrParseError;
 
+#[derive(Debug, PartialEq, Eq, Clone, Copy)]
+pub enum Tile {
+    Rock,
+    Wall,
+    Empty,
+}
+
 peg::parser! { pub grammar day14_parser() for str {
     rule number() -> usize
         = n:$(['0'..='9']+) { n.parse().expect(&format!("Was expecting a number string {}", n)[..])}
-    pub rule parse() -> Vec<usize>
-        = lines:number() ++ ("\n" +) "\n" * { lines }
+    rule rock() -> Tile
+        = "O" { Tile::Rock }
+    rule wall() -> Tile
+        = "#" { Tile::Wall }
+    rule empty() -> Tile
+        = "." { Tile::Empty }
+    rule tile() -> Tile
+        = tile:(rock() / wall() / empty()) { tile }
+    rule line() -> Vec<Tile>
+        = line:tile() ++ "" { line }
+    pub rule parse() -> Vec<Vec<Tile>>
+        = lines:line() ++ ("\n" +) "\n" * { lines }
 }}
 
-pub fn parse_data(input: &str) -> Result<Vec<usize>, AOCFileOrParseError> {
+pub fn parse_data(input: &str) -> Result<Vec<Vec<Tile>>, AOCFileOrParseError> {
     if let Ok(ret) = day14_parser::parse(input) {
         Ok(ret)
     } else {
@@ -26,7 +43,6 @@ mod test {
     fn test_parse() {
         let input_str = read_input_file("data/test_data.txt").unwrap();
         let actual = day14_parser::parse(&input_str).expect("Should parse successfully");
-        let expected: Vec<usize> = vec![];
-        assert_eq!(expected, actual)
+        assert_eq!(10, actual.len())
     }
 }
