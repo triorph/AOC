9bb18f0995f5b7a526bd65d0173048f438b6e8bf
Michael Walsh <mwalsh2@atlassian.com>
2024-12-06 22:02:40 +1300

    Day 6 done, although slower than I'd like (especially this early). Day 7 templated

--- aoc_2024/day6/src/lib.rs
+++ aoc_2024/day6/src/lib.rs
@@ -1,16 +1,20 @@
 mod parser;
+use std::collections::HashSet;
+
 use crate::parser::parse_data;
-use aoc_helpers::{read_input_file, AOCCalculator, AOCFileOrParseError};
+use aoc_helpers::{point2d::Point2D, read_input_file, AOCCalculator, AOCFileOrParseError};
+use itertools::Itertools;
+use parser::MapTile;
 
 #[derive(Debug, Clone, PartialEq, Eq)]
 pub struct Day6 {
-    data: Vec<usize>,
+    map: Vec<Vec<MapTile>>,
 }
 
 impl AOCCalculator for Day6 {
     fn new(filename: &str) -> Result<Day6, AOCFileOrParseError> {
         Ok(Day6 {
-            data: parse_data(&read_input_file(filename)?)?,
+            map: parse_data(&read_input_file(filename)?)?,
         })
     }
 
@@ -21,12 +25,95 @@ impl AOCCalculator for Day6 {
 }
 
 impl Day6 {
+    fn get_player_and_rocks(&self) -> (Option<Point2D>, HashSet<Point2D>) {
+        (0..self.map.len())
+            .cartesian_product(0..self.map[0].len())
+            .fold(
+                (None, HashSet::new()),
+                |(player, mut rocks), (y, x)| match self.map[y][x] {
+                    MapTile::Rock => {
+                        rocks.insert(Point2D::from_usize(x, y));
+                        (player, rocks)
+                    }
+                    MapTile::Player => (Some(Point2D::from_usize(x, y)), rocks),
+                    _ => (player, rocks),
+                },
+            )
+    }
+
+    fn rotate_direction(&self, direction: &Point2D) -> Point2D {
+        Point2D {
+            x: -direction.y,
+            y: direction.x,
+        }
+    }
+
+    fn within_bounds(&self, player: &Point2D) -> bool {
+        (0..self.map.len() as isize).contains(&player.y)
+            && (0..self.map[0].len() as isize).contains(&player.x)
+    }
+
+    fn get_guard_positions(&self) -> HashSet<Point2D> {
+        let (player, rocks) = self.get_player_and_rocks();
+        let mut player = player.expect("Must have a player start position");
+        let mut direction = Point2D { x: 0, y: -1 };
+        let mut visited_points = HashSet::new();
+        visited_points.insert(player);
+        while self.within_bounds(&player) {
+            let next_pos = player + direction;
+            if rocks.contains(&next_pos) {
+                direction = self.rotate_direction(&direction);
+            } else {
+                player = next_pos;
+                visited_points.insert(player);
+            }
+        }
+        visited_points
+    }
+
     fn calculate_day_a(&self) -> usize {
-        0
+        self.get_guard_positions().len() - 1 // remove the out of bounds entry
+    }
+
+    fn check_for_loop(
+        &self,
+        player: &Point2D,
+        rocks: &HashSet<Point2D>,
+        new_rock: &Point2D,
+    ) -> bool {
+        let mut rocks = rocks.clone();
+        rocks.insert(*new_rock);
+        let mut player = *player;
+        let mut direction = Point2D { x: 0, y: -1 };
+        let mut visited_points = HashSet::new();
+        visited_points.insert((player, direction));
+        while self.within_bounds(&player) {
+            let next_pos = player + direction;
+            if rocks.contains(&next_pos) {
+                direction = self.rotate_direction(&direction);
+            } else {
+                player = next_pos;
+                if visited_points.contains(&(player, direction)) {
+                    return true;
+                } else {
+                    visited_points.insert((player, direction));
+                }
+            }
+        }
+        false
     }
 
     fn calculate_day_b(&self) -> usize {
-        0
+        let (player, rocks) = self.get_player_and_rocks();
+        let player = player.expect("Must have a player start position");
+        self.get_guard_positions()
+            .iter()
+            .filter(|new_rock| {
+                **new_rock != player
+                    && !rocks.contains(new_rock)
+                    && self.check_for_loop(&player, &rocks, new_rock)
+            })
+            .count()
     }
 }
 
@@ -37,33 +124,33 @@ mod tests {
 
     #[test]
     fn test_calculate_day_a() {
-        let day6 = Day6::new("data/test_data.txt").unwrap();
-        let expected = 0;
-        let actual = day6.calculate_day_a();
+        let skeleton = Day6::new("data/test_data.txt").unwrap();
+        let expected = 41;
+        let actual = skeleton.calculate_day_a();
         assert_eq!(expected, actual);
     }
 
     #[test]
     fn test_calculate_day_b() {
-        let day6 = Day6::new("data/test_data.txt").unwrap();
-        let expected = 0;
-        let actual = day6.calculate_day_b();
+        let skeleton = Day6::new("data/test_data.txt").unwrap();
+        let expected = 6;
+        let actual = skeleton.calculate_day_b();
         assert_eq!(expected, actual);
     }
 
     #[test]
     fn test_real_input_calculate_day_a() {
-        let day6 = Day6::new("data/input_data.txt").unwrap();
-        let expected = 0;
-        let actual = day6.calculate_day_a();
+        let skeleton = Day6::new("data/input_data.txt").unwrap();
+        let expected = 4647;
+        let actual = skeleton.calculate_day_a();
         assert_eq!(expected, actual);
     }
 
     #[test]
     fn test_real_input_calculate_day_b() {
-        let day6 = Day6::new("data/input_data.txt").unwrap();
-        let expected = 0;
-        let actual = day6.calculate_day_b();
+        let skeleton = Day6::new("data/input_data.txt").unwrap();
+        let expected = 1723;
+        let actual = skeleton.calculate_day_b();
         assert_eq!(expected, actual);
     }
 }
--- aoc_2024/day6/src/parser.rs
+++ aoc_2024/day6/src/parser.rs
@@ -1,14 +1,31 @@
 extern crate peg;
 use aoc_helpers::AOCFileOrParseError;
 
+#[derive(Debug, PartialEq, Eq, Clone)]
+pub enum MapTile {
+    Rock,
+    Player,
+    Empty,
+}
+
 peg::parser! { pub grammar day6_parser() for str {
     rule number() -> usize
         = n:$(['0'..='9']+) { n.parse().expect(&format!("Was expecting a number string {}", n)[..])}
-    pub rule parse() -> Vec<usize>
-        = lines:number() ** ("\n" +) "\n" * { lines }
+    rule rock() -> MapTile
+        = "#" { MapTile::Rock }
+    rule player() -> MapTile
+        = "^" { MapTile::Player }
+    rule empty() -> MapTile
+        = "." { MapTile::Empty }
+    rule map_tile() -> MapTile
+        = map_tile:(rock() / player() / empty()) { map_tile }
+    rule line() -> Vec<MapTile>
+        = line:map_tile() ++ "" { line }
+    pub rule parse() -> Vec<Vec<MapTile>>
+        = lines:line() ++ ("\n" +) "\n" * { lines }
 }}
 
-pub fn parse_data(input: &str) -> Result<Vec<usize>, AOCFileOrParseError> {
+pub fn parse_data(input: &str) -> Result<Vec<Vec<MapTile>>, AOCFileOrParseError> {
     if let Ok(ret) = day6_parser::parse(input) {
         Ok(ret)
     } else {
@@ -26,7 +43,128 @@ mod test {
     fn test_parse() {
         let input_str = read_input_file("data/test_data.txt").unwrap();
         let actual = day6_parser::parse(&input_str).expect("Should parse successfully");
-        let expected: Vec<usize> = vec![];
+        let expected: Vec<Vec<MapTile>> = vec![
+            vec![
+                MapTile::Empty,
+                MapTile::Empty,
+                MapTile::Empty,
+                MapTile::Empty,
+                MapTile::Rock,
+                MapTile::Empty,
+                MapTile::Empty,
+                MapTile::Empty,
+                MapTile::Empty,
+                MapTile::Empty,
+            ],
+            vec![
+                MapTile::Empty,
+                MapTile::Empty,
+                MapTile::Empty,
+                MapTile::Empty,
+                MapTile::Empty,
+                MapTile::Empty,
+                MapTile::Empty,
+                MapTile::Empty,
+                MapTile::Empty,
+                MapTile::Rock,
+            ],
+            vec![
+                MapTile::Empty,
+                MapTile::Empty,
+                MapTile::Empty,
+                MapTile::Empty,
+                MapTile::Empty,
+                MapTile::Empty,
+                MapTile::Empty,
+                MapTile::Empty,
+                MapTile::Empty,
+                MapTile::Empty,
+            ],
+            vec![
+                MapTile::Empty,
+                MapTile::Empty,
+                MapTile::Rock,
+                MapTile::Empty,
+                MapTile::Empty,
+                MapTile::Empty,
+                MapTile::Empty,
+                MapTile::Empty,
+                MapTile::Empty,
+                MapTile::Empty,
+            ],
+            vec![
+                MapTile::Empty,
+                MapTile::Empty,
+                MapTile::Empty,
+                MapTile::Empty,
+                MapTile::Empty,
+                MapTile::Empty,
+                MapTile::Empty,
+                MapTile::Rock,
+                MapTile::Empty,
+                MapTile::Empty,
+            ],
+            vec![
+                MapTile::Empty,
+                MapTile::Empty,
+                MapTile::Empty,
+                MapTile::Empty,
+                MapTile::Empty,
+                MapTile::Empty,
+                MapTile::Empty,
+                MapTile::Empty,
+                MapTile::Empty,
+                MapTile::Empty,
+            ],
+            vec![
+                MapTile::Empty,
+                MapTile::Rock,
+                MapTile::Empty,
+                MapTile::Empty,
+                MapTile::Player,
+                MapTile::Empty,
+                MapTile::Empty,
+                MapTile::Empty,
+                MapTile::Empty,
+                MapTile::Empty,
+            ],
+            vec![
+                MapTile::Empty,
+                MapTile::Empty,
+                MapTile::Empty,
+                MapTile::Empty,
+                MapTile::Empty,
+                MapTile::Empty,
+                MapTile::Empty,
+                MapTile::Empty,
+                MapTile::Rock,
+                MapTile::Empty,
+            ],
+            vec![
+                MapTile::Rock,
+                MapTile::Empty,
+                MapTile::Empty,
+                MapTile::Empty,
+                MapTile::Empty,
+                MapTile::Empty,
+                MapTile::Empty,
+                MapTile::Empty,
+                MapTile::Empty,
+                MapTile::Empty,
+            ],
+            vec![
+                MapTile::Empty,
+                MapTile::Empty,
+                MapTile::Empty,
+                MapTile::Empty,
+                MapTile::Empty,
+                MapTile::Empty,
+                MapTile::Rock,
+                MapTile::Empty,
+                MapTile::Empty,
+                MapTile::Empty,
+            ],
+        ];
         assert_eq!(expected, actual)
     }
 }
