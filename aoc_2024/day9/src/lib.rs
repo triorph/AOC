666888f61fdd8edb960610c3429b0ad8cd434c71
Michael Walsh <mwalsh2@atlassian.com>
2024-12-11 15:45:58 +1100

    Day9 done but horribly ugly. Tidy up at some point.

--- aoc_2024/day9/src/lib.rs
+++ aoc_2024/day9/src/lib.rs
@@ -1,5 +1,5 @@
 mod parser;
-use std::collections::HashMap;
+use std::collections::VecDeque;
 
 use crate::parser::parse_data;
 use aoc_helpers::{read_input_file, AOCCalculator, AOCFileOrParseError};
@@ -23,63 +23,131 @@ impl AOCCalculator for Day9 {
 }
 
 impl Day9 {
-    fn calculate_checksum(&self) -> usize {
-        0
+    fn calculate_checksum(&self, defragged_sector: &[(Option<usize>, usize, usize)]) -> usize {
+        defragged_sector.iter().fold(0, |checksum, sector_entry| {
+            let (sector_id, sector_start, sector_end) = sector_entry;
+            if let Some(sector_id) = sector_id {
+                checksum
+                    + (*sector_start..*sector_end)
+                        .map(|i| (i * sector_id))
+                        .sum::<usize>()
+            } else {
+                checksum
+            }
+        })
     }
 
-    fn build_sector_map(&self) -> (Vec<(usize, usize, usize)>, Vec<(usize, usize)>) {
+    fn build_sector_map(&self) -> VecDeque<(Option<usize>, usize, usize)> {
         self.data
             .chunks(2)
             .enumerate()
             .fold(
-                (0, (vec![], vec![])),
-                |(counter, (mut used_sectors, mut free_sectors)), (i, x)| {
-                    used_sectors.push((i, counter, (counter + x[0])));
-                    free_sectors.push((counter + x[0], counter + x[0] + x[1]));
-                    (counter + x[0] + x[1], (used_sectors, free_sectors))
+                (0, VecDeque::new()),
+                |(counter, mut sector_ranges), (i, x)| {
+                    sector_ranges.push_back((Some(i), counter, counter + x[0]));
+                    if x.len() == 2 {
+                        sector_ranges.push_back((None, counter + x[0], counter + x[0] + x[1]));
+                        (counter + x[0] + x[1], sector_ranges)
+                    } else {
+                        (counter + x[0], sector_ranges)
+                    }
                 },
             )
             .1
     }
 
-    fn get_largest_value(&self, used_sectors: &[(usize, usize, usize)]) -> usize {
-        used_sectors
-            .iter()
-            .filter(|(_id, min, max)| min < max)
-            .map(|(_id, _min, max)| *max)
-            .max()
-            .unwrap_or(0)
-    }
-
-    fn get_smallest_empty(&self, free_sectors: &[(usize, usize)]) -> Option<(usize, usize)> {
-        free_sectors
-            .iter()
-            .filter(|(min, max)| min < max)
-            .map(|(min, max)| (*min, *max))
-            .min_by(|(min_a, _), (min_b, _)| match (min_a, min_b) {
-                (min_a, min_b) if min_a < min_b => std::cmp::Ordering::Less,
-                (min_a, min_b) if min_b < min_a => std::cmp::Ordering::Greater,
-                _ => std::cmp::Ordering::Equal,
-            })
-    }
-
-    fn more_to_do(
-        &self,
-        used_sectors: &[(usize, usize, usize)],
-        free_sectors: &[(usize, usize)],
-    ) -> bool {
-        let smallest = self.get_smallest_empty(&free_sectors);
-        smallest.is_some() && self.get_largest_value(&used_sectors) > smallest.unwrap().0
-    }
-
     fn calculate_day_a(&self) -> usize {
-        let (mut used_sectors, mut free_sectors) = self.build_sector_map();
-        while self.more_to_do(&used_sectors, &free_sectors) {}
-        self.calculate_checksum()
+        let mut ret = vec![];
+        let mut sectors = self.build_sector_map();
+        let mut current_sector = sectors.pop_front();
+        let mut sector_to_move = sectors.pop_back();
+        while current_sector.is_some() && sector_to_move.is_some() {
+            let (id, start, end) = current_sector.unwrap();
+            let (other_id, other_start, other_end) = sector_to_move.unwrap();
+            if other_id.is_none() {
+                sector_to_move = sectors.pop_back();
+            } else if id.is_some() {
+                ret.push(current_sector.unwrap());
+                current_sector = sectors.pop_front();
+            } else {
+                let size_moved = usize::min(end - start, other_end - other_start);
+                ret.push((other_id, start, start + size_moved));
+                let start = start + size_moved;
+                let other_end = other_end - size_moved;
+                if start == end {
+                    current_sector = sectors.pop_front();
+                } else {
+                    current_sector = Some((id, start, end));
+                }
+                if other_start == other_end {
+                    sector_to_move = sectors.pop_back();
+                } else {
+                    sector_to_move = Some((other_id, other_start, other_end))
+                }
+            }
+        }
+        if let Some((id, start, end)) = sector_to_move {
+            if id.is_some() {
+                ret.push((id, start, end))
+            }
+        }
+
+        self.calculate_checksum(&ret)
     }
 
     fn calculate_day_b(&self) -> usize {
-        0
+        let mut sector_map = self.build_sector_map();
+        let max_value = sector_map
+            .iter()
+            .map(|(id, _, _)| id.unwrap_or(0))
+            .max()
+            .unwrap();
+        for id in (0..=max_value).rev() {
+            let sector_entry = sector_map
+                .iter()
+                .enumerate()
+                .find(|(_, val)| val.0 == Some(id))
+                .unwrap_or_else(|| {
+                    panic!(
+                        "for some reason entry {:?} doesn't exist, {:?}",
+                        id, sector_map
+                    );
+                })
+                .0;
+            sector_map[sector_entry].0 = None;
+            let size = sector_map[sector_entry].2 - sector_map[sector_entry].1;
+            let earliest_placement = sector_map
+                .iter()
+                .enumerate()
+                .find(
+                    |(_, (free_sector_id, free_sector_start, free_sector_end))| {
+                        free_sector_id.is_none() && free_sector_end - free_sector_start >= size
+                    },
+                )
+                .expect("Will at least go back to where it was")
+                .0;
+            let (_, free_sector_start, free_sector_end) = sector_map[earliest_placement];
+            let free_size = free_sector_end - free_sector_start;
+            sector_map[earliest_placement].0 = Some(id);
+            sector_map[earliest_placement].2 = free_sector_start + size;
+            if free_size > size {
+                // create a new free sector from the remaining stuff
+                // (do we need to join to the next, surely not?)
+                sector_map.insert(
+                    earliest_placement + 1,
+                    (
+                        None,
+                        free_sector_start + size,
+                        free_sector_start + free_size,
+                    ),
+                );
+            }
+        }
+        self.calculate_checksum(
+            &sector_map
+                .into_iter()
+                .collect::<Vec<(Option<usize>, usize, usize)>>(),
+        )
     }
 }
 
@@ -99,7 +167,7 @@ mod tests {
     #[test]
     fn test_calculate_day_b() {
         let day9 = Day9::new("data/test_data.txt").unwrap();
-        let expected = 0;
+        let expected = 2858;
         let actual = day9.calculate_day_b();
         assert_eq!(expected, actual);
     }
@@ -107,7 +175,7 @@ mod tests {
     #[test]
     fn test_real_input_calculate_day_a() {
         let day9 = Day9::new("data/input_data.txt").unwrap();
-        let expected = 0;
+        let expected = 6241633730082;
         let actual = day9.calculate_day_a();
         assert_eq!(expected, actual);
     }
@@ -115,8 +183,33 @@ mod tests {
     #[test]
     fn test_real_input_calculate_day_b() {
         let day9 = Day9::new("data/input_data.txt").unwrap();
-        let expected = 0;
+        let expected = 6265268809555;
         let actual = day9.calculate_day_b();
+        assert_eq!(expected, actual);
+    }
+
+    #[test]
+    fn test_calculate_checksum() {
+        let day9 = Day9::new("data/test_data.txt").unwrap();
+        let input = [
+            (Some(0), 0, 2),
+            (Some(9), 2, 4),
+            (Some(2), 4, 5),
+            (Some(1), 5, 8),
+            (Some(7), 8, 11),
+            (None, 11, 12),
+            (Some(4), 12, 14),
+            (None, 14, 15),
+            (Some(3), 15, 18),
+            (None, 18, 22),
+            (Some(5), 22, 26),
+            (None, 26, 27),
+            (Some(6), 27, 31),
+            (None, 31, 36),
+            (Some(8), 36, 40),
+        ];
+        let actual = day9.calculate_checksum(&input);
+        let expected = 2858;
         assert_eq!(expected, actual);
     }
 }
