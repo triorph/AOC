cfe64ef2bc32f795c4311845c7a56b63b99f88cf
Michael Walsh <m.walsh@omni-nde.com>
2024-12-28 10:34:58 +1300

    Day19 done

--- aoc_2024/day19/src/lib.rs
+++ aoc_2024/day19/src/lib.rs
@@ -1,17 +1,19 @@
 mod parser;
+use std::collections::{BinaryHeap, HashMap, VecDeque};
+
 use crate::parser::parse_data;
 use aoc_helpers::{read_input_file, AOCCalculator, AOCFileOrParseError};
 
 #[derive(Debug, Clone, PartialEq, Eq)]
 pub struct Day19 {
-    data: Vec<usize>,
+    patterns: Vec<String>,
+    desired: Vec<String>,
 }
 
 impl AOCCalculator for Day19 {
     fn new(filename: &str) -> Result<Day19, AOCFileOrParseError> {
-        Ok(Day19 {
-            data: parse_data(&read_input_file(filename)?)?,
-        })
+        let (patterns, desired) = parse_data(&read_input_file(filename)?)?;
+        Ok(Day19 { patterns, desired })
     }
 
     fn print_results(&self, name: &str) {
@@ -21,12 +23,54 @@ impl AOCCalculator for Day19 {
 }
 
 impl Day19 {
+    fn is_desired_possible(&self, desired: &str) -> bool {
+        let mut to_explore = VecDeque::new();
+        to_explore.push_back(0);
+        while let Some(explore) = to_explore.pop_front() {
+            for pattern in self.patterns.iter() {
+                if desired[explore..desired.len()].starts_with(pattern) {
+                    if explore + pattern.len() == desired.len() {
+                        return true;
+                    }
+                    if !to_explore.contains(&(explore + pattern.len())) {
+                        to_explore.push_back(explore + pattern.len())
+                    }
+                }
+            }
+        }
+        false
+    }
+
+    fn ways_to_end(&self, desired: &str) -> usize {
+        let mut ways_to_point: HashMap<usize, usize> = HashMap::new();
+        ways_to_point.insert(0, 1);
+        for i in 0..desired.len() {
+            let paths_here = *ways_to_point.get(&i).unwrap_or(&0);
+            if paths_here == 0 {
+                continue;
+            }
+            for pattern in self.patterns.iter() {
+                if desired[i..desired.len()].starts_with(pattern) {
+                    let next = i + pattern.len();
+                    *ways_to_point.entry(next).or_default() += paths_here;
+                }
+            }
+        }
+        *ways_to_point.get(&(desired.len())).unwrap_or(&0)
+    }
+
     fn calculate_day_a(&self) -> usize {
-        0
+        self.desired
+            .iter()
+            .filter(|desired| self.is_desired_possible(desired))
+            .count()
     }
 
     fn calculate_day_b(&self) -> usize {
-        0
+        self.desired
+            .iter()
+            .map(|desired| self.ways_to_end(desired))
+            .sum()
     }
 }
 
@@ -38,7 +82,7 @@ mod tests {
     #[test]
     fn test_calculate_day_a() {
         let day19 = Day19::new("data/test_data.txt").unwrap();
-        let expected = 0;
+        let expected = 6;
         let actual = day19.calculate_day_a();
         assert_eq!(expected, actual);
     }
@@ -46,7 +90,7 @@ mod tests {
     #[test]
     fn test_calculate_day_b() {
         let day19 = Day19::new("data/test_data.txt").unwrap();
-        let expected = 0;
+        let expected = 16;
         let actual = day19.calculate_day_b();
         assert_eq!(expected, actual);
     }
@@ -54,7 +98,7 @@ mod tests {
     #[test]
     fn test_real_input_calculate_day_a() {
         let day19 = Day19::new("data/input_data.txt").unwrap();
-        let expected = 0;
+        let expected = 365;
         let actual = day19.calculate_day_a();
         assert_eq!(expected, actual);
     }
@@ -62,7 +106,7 @@ mod tests {
     #[test]
     fn test_real_input_calculate_day_b() {
         let day19 = Day19::new("data/input_data.txt").unwrap();
-        let expected = 0;
+        let expected = 730121486795169;
         let actual = day19.calculate_day_b();
         assert_eq!(expected, actual);
     }
--- aoc_2024/day19/src/parser.rs
+++ aoc_2024/day19/src/parser.rs
@@ -2,13 +2,21 @@ extern crate peg;
 use aoc_helpers::AOCFileOrParseError;
 
 peg::parser! { pub grammar day19_parser() for str {
+    rule stripe() -> char
+        = stripe:(['a'..='z']) { stripe }
+    rule towel() -> String
+        = towel:stripe() ++ "" { towel.into_iter().collect() }
+    rule towel_patterns() -> Vec<String>
+        = towel_patterns:towel() ++ ", " { towel_patterns }
+    rule desired_designs() -> Vec<String>
+        = desired_designs:towel() ++ "\n" { desired_designs }
     rule number() -> usize
         = n:$(['0'..='9']+) { n.parse().expect(&format!("Was expecting a number string {}", n)[..])}
-    pub rule parse() -> Vec<usize>
-        = lines:number() ++ ("\n" +) "\n" * { lines }
+    pub rule parse() -> (Vec<String>, Vec<String>)
+        = towel_patterns:towel_patterns() "\n"+ desired_designs:desired_designs() "\n"* { (towel_patterns, desired_designs) }
 }}
 
-pub fn parse_data(input: &str) -> Result<Vec<usize>, AOCFileOrParseError> {
+pub fn parse_data(input: &str) -> Result<(Vec<String>, Vec<String>), AOCFileOrParseError> {
     if let Ok(ret) = day19_parser::parse(input) {
         Ok(ret)
     } else {
@@ -26,7 +34,28 @@ mod test {
     fn test_parse() {
         let input_str = read_input_file("data/test_data.txt").unwrap();
         let actual = day19_parser::parse(&input_str).expect("Should parse successfully");
-        let expected: Vec<usize> = vec![];
+        let expected = (
+            vec![
+                String::from("r"),
+                String::from("wr"),
+                String::from("b"),
+                String::from("g"),
+                String::from("bwu"),
+                String::from("rb"),
+                String::from("gb"),
+                String::from("br"),
+            ],
+            vec![
+                String::from("brwrr"),
+                String::from("bggr"),
+                String::from("gbbr"),
+                String::from("rrbgbr"),
+                String::from("ubwu"),
+                String::from("bwurrg"),
+                String::from("brgr"),
+                String::from("bbrgwb"),
+            ],
+        );
         assert_eq!(expected, actual)
     }
 }
