76606344f3613aa2148cc92b0e8af3a948cd5821
Michael Walsh <m.walsh@omni-nde.com>
2025-01-04 11:41:55 +1300

    Day23 done

--- aoc_2024/day23/src/lib.rs
+++ aoc_2024/day23/src/lib.rs
@@ -1,16 +1,18 @@
 mod parser;
+use std::collections::{HashMap, HashSet};
+
 use crate::parser::parse_data;
 use aoc_helpers::{read_input_file, AOCCalculator, AOCFileOrParseError};
 
 #[derive(Debug, Clone, PartialEq, Eq)]
 pub struct Day23 {
-    data: Vec<usize>,
+    edges: Vec<(String, String)>,
 }
 
 impl AOCCalculator for Day23 {
     fn new(filename: &str) -> Result<Day23, AOCFileOrParseError> {
         Ok(Day23 {
-            data: parse_data(&read_input_file(filename)?)?,
+            edges: parse_data(&read_input_file(filename)?)?,
         })
     }
 
@@ -21,12 +23,92 @@ impl AOCCalculator for Day23 {
 }
 
 impl Day23 {
-    fn calculate_day_a(&self) -> usize {
-        0
+    fn build_connected_map(&self) -> HashMap<String, HashSet<String>> {
+        let mut connected: HashMap<String, HashSet<String>> = HashMap::new();
+        for (left, right) in self.edges.iter() {
+            connected
+                .entry(left.clone())
+                .or_default()
+                .insert(right.clone());
+            connected
+                .entry(right.clone())
+                .or_default()
+                .insert(left.clone());
+        }
+        connected
     }
 
-    fn calculate_day_b(&self) -> usize {
-        0
+    fn find_largest_connection(&self) -> HashSet<String> {
+        let connected = self.build_connected_map();
+        let mut possible_groups: Vec<HashSet<String>> = connected
+            .keys()
+            .map(|x| HashSet::from([x.clone()]))
+            .collect();
+        let mut biggest_path = 1;
+        loop {
+            let mut next = vec![];
+            let mut change_made = false;
+            for group in possible_groups.clone().into_iter() {
+                let mut all_neighbours = HashSet::new();
+                for item in group.iter() {
+                    all_neighbours.extend(connected.get(item).unwrap());
+                }
+                let mut found = false;
+                for neighbour in all_neighbours.iter() {
+                    if group
+                        .iter()
+                        .all(|n| connected.get(*neighbour).unwrap().contains(n))
+                    {
+                        found = true;
+                        change_made = true;
+                        let mut next_group = group.clone();
+                        next_group.insert((*neighbour).clone());
+                        biggest_path = next_group.len().max(biggest_path);
+                        next.push(next_group);
+                        break;
+                    }
+                }
+                if !found && group.len() == biggest_path {
+                    next.push(group.clone());
+                }
+            }
+            if !change_made {
+                return next.into_iter().find(|n| n.len() == biggest_path).unwrap();
+            }
+            possible_groups = next;
+        }
+    }
+
+    fn find_all_connections(&self) -> HashSet<Vec<String>> {
+        let connected = self.build_connected_map();
+        let mut ret = HashSet::new();
+        for (left, right) in self.edges.iter() {
+            let overlap: HashSet<String> = connected
+                .get(left)
+                .unwrap()
+                .intersection(connected.get(right).unwrap())
+                .cloned()
+                .collect();
+            for node in overlap.iter() {
+                let mut connection = vec![left.clone(), right.clone(), node.clone()];
+                connection.sort();
+                ret.insert(connection);
+            }
+        }
+        ret
+    }
+    fn calculate_day_a(&self) -> usize {
+        self.find_all_connections()
+            .into_iter()
+            .filter(|x| x.iter().any(|inner| inner.starts_with('t')))
+            .count()
+    }
+
+    fn calculate_day_b(&self) -> String {
+        let best_path = self.find_largest_connection();
+        let mut best_path = Vec::from_iter(best_path);
+        best_path.sort();
+        best_path.join(",")
     }
 }
 
@@ -38,7 +120,7 @@ mod tests {
     #[test]
     fn test_calculate_day_a() {
         let day23 = Day23::new("data/test_data.txt").unwrap();
-        let expected = 0;
+        let expected = 7;
         let actual = day23.calculate_day_a();
         assert_eq!(expected, actual);
     }
@@ -46,7 +128,7 @@ mod tests {
     #[test]
     fn test_calculate_day_b() {
         let day23 = Day23::new("data/test_data.txt").unwrap();
-        let expected = 0;
+        let expected = "co,de,ka,ta".to_string();
         let actual = day23.calculate_day_b();
         assert_eq!(expected, actual);
     }
@@ -54,7 +136,7 @@ mod tests {
     #[test]
     fn test_real_input_calculate_day_a() {
         let day23 = Day23::new("data/input_data.txt").unwrap();
-        let expected = 0;
+        let expected = 998;
         let actual = day23.calculate_day_a();
         assert_eq!(expected, actual);
     }
@@ -62,7 +144,7 @@ mod tests {
     #[test]
     fn test_real_input_calculate_day_b() {
         let day23 = Day23::new("data/input_data.txt").unwrap();
-        let expected = 0;
+        let expected = "cc,ff,fh,fr,ny,oa,pl,rg,uj,wd,xn,xs,zw".to_string();
         let actual = day23.calculate_day_b();
         assert_eq!(expected, actual);
     }
--- aoc_2024/day23/src/parser.rs
+++ aoc_2024/day23/src/parser.rs
@@ -4,11 +4,15 @@ use aoc_helpers::AOCFileOrParseError;
 peg::parser! { pub grammar day23_parser() for str {
     rule number() -> usize
         = n:$(['0'..='9']+) { n.parse().expect(&format!("Was expecting a number string {}", n)[..])}
-    pub rule parse() -> Vec<usize>
-        = lines:number() ++ ("\n" +) "\n" * { lines }
+    rule node() -> String
+        = node:$(['a'..='z']+) { node.to_string() }
+    rule edge() -> (String, String)
+        = left:node() "-" right:node() { (left, right) }
+    pub rule parse() -> Vec<(String, String)>
+        = lines:edge() ++ ("\n" +) "\n" * { lines }
 }}
 
-pub fn parse_data(input: &str) -> Result<Vec<usize>, AOCFileOrParseError> {
+pub fn parse_data(input: &str) -> Result<Vec<(String, String)>, AOCFileOrParseError> {
     if let Ok(ret) = day23_parser::parse(input) {
         Ok(ret)
     } else {
@@ -26,7 +30,40 @@ mod test {
     fn test_parse() {
         let input_str = read_input_file("data/test_data.txt").unwrap();
         let actual = day23_parser::parse(&input_str).expect("Should parse successfully");
-        let expected: Vec<usize> = vec![];
+        let expected: Vec<(String, String)> = vec![
+            (String::from("kh"), String::from("tc")),
+            (String::from("qp"), String::from("kh")),
+            (String::from("de"), String::from("cg")),
+            (String::from("ka"), String::from("co")),
+            (String::from("yn"), String::from("aq")),
+            (String::from("qp"), String::from("ub")),
+            (String::from("cg"), String::from("tb")),
+            (String::from("vc"), String::from("aq")),
+            (String::from("tb"), String::from("ka")),
+            (String::from("wh"), String::from("tc")),
+            (String::from("yn"), String::from("cg")),
+            (String::from("kh"), String::from("ub")),
+            (String::from("ta"), String::from("co")),
+            (String::from("de"), String::from("co")),
+            (String::from("tc"), String::from("td")),
+            (String::from("tb"), String::from("wq")),
+            (String::from("wh"), String::from("td")),
+            (String::from("ta"), String::from("ka")),
+            (String::from("td"), String::from("qp")),
+            (String::from("aq"), String::from("cg")),
+            (String::from("wq"), String::from("ub")),
+            (String::from("ub"), String::from("vc")),
+            (String::from("de"), String::from("ta")),
+            (String::from("wq"), String::from("aq")),
+            (String::from("wq"), String::from("vc")),
+            (String::from("wh"), String::from("yn")),
+            (String::from("ka"), String::from("de")),
+            (String::from("kh"), String::from("ta")),
+            (String::from("co"), String::from("tc")),
+            (String::from("wh"), String::from("qp")),
+            (String::from("tb"), String::from("vc")),
+            (String::from("td"), String::from("yn")),
+        ];
         assert_eq!(expected, actual)
     }
 }
