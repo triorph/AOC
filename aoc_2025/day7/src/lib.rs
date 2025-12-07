55c0067a14ce9f62e436bed1317b5d08f076b28a
Michael Walsh <mwalsh2@atlassian.com>
2025-12-08 09:07:44 +1300

    Day 7 done

--- aoc_2025/day7/src/lib.rs
+++ aoc_2025/day7/src/lib.rs
@@ -1,10 +1,8 @@
 mod parser;
-use std::collections::HashSet;
+use std::collections::{HashMap, HashSet};
 
 use crate::parser::parse_data;
-use aoc_helpers::{
-    hash_utils::FromVec, point2d::Point2D, read_input_file, AOCCalculator, AOCFileOrParseError,
-};
+use aoc_helpers::{point2d::Point2D, read_input_file, AOCCalculator, AOCFileOrParseError};
 
 #[derive(Debug, Clone, PartialEq, Eq)]
 pub struct Day7 {
@@ -25,55 +23,62 @@ impl AOCCalculator for Day7 {
 }
 
 impl Day7 {
-    fn move_beams(&self, beam_positions: &HashSet<Point2D>) -> HashSet<Point2D> {
-        beam_positions
-            .union(
-                &beam_positions
-                    .clone()
-                    .difference(&self.splitters)
-                    .map(|beam_pos| *beam_pos + Point2D { x: 0, y: 1 })
-                    .collect::<HashSet<Point2D>>(),
-            )
-            .copied()
-            .collect()
-    }
-
-    fn split_beams(&self, beam_positions: &HashSet<Point2D>) -> HashSet<Point2D> {
-        beam_positions
-            .union(
-                &self
-                    .splitters
-                    .clone()
-                    .difference(beam_positions)
-                    .flat_map(|splitter_pos| {
-                        vec![
-                            *splitter_pos + Point2D { x: -1, y: 0 },
-                            *splitter_pos + Point2D { x: 1, y: 0 },
-                        ]
-                    })
-                    .collect::<HashSet<Point2D>>(),
-            )
-            .copied()
-            .collect()
-    }
-
-    fn next_splitters(&self, beam_positions: &HashSet<Point2D>) -> HashSet<Point2D> {
-        self.split_beams(&self.move_beams(beam_positions))
-    }
-
-    fn all_moves(&self) -> HashSet<Point2D> {
-        let max = self.splitters.iter().map(|p| p.y).max().unwrap() as usize;
-        (0..=max).fold(HashSet::from_vec(&[self.start]), |acc, _| {
-            self.next_splitters(&acc)
-        })
-    }
-
     fn calculate_day_a(&self) -> usize {
-        self.all_moves().intersection(&self.splitters).count()
+        // find how many tachyon lines overlap splitters
+        self.get_all_tachyon_lines()
+            .values()
+            .map(|line| line.keys().copied().collect::<HashSet<Point2D>>())
+            .reduce(|a, b| a.union(&b).copied().collect::<HashSet<Point2D>>())
+            .unwrap()
+            .intersection(&self.splitters)
+            .count()
     }
 
     fn calculate_day_b(&self) -> usize {
-        0
+        // sum how many tachyon lines made it to the final line
+        let all_moves = self.get_all_tachyon_lines();
+        all_moves[all_moves.keys().max().unwrap()].values().sum()
+    }
+
+    fn get_all_tachyon_lines(&self) -> HashMap<usize, HashMap<Point2D, usize>> {
+        let max = self.splitters.iter().map(|p| p.y).max().unwrap() as usize;
+        let start = HashMap::from([(0, HashMap::from([(self.start, 1)]))]);
+        (0..=max).fold(start, |mut acc, i| {
+            acc.insert(i + 1, self.next_tachyon_line(&acc[&i]));
+            acc
+        })
+    }
+
+    fn next_tachyon_line(
+        &self,
+        beam_positions: &HashMap<Point2D, usize>,
+    ) -> HashMap<Point2D, usize> {
+        self.split_beams(&self.move_beams(beam_positions))
+    }
+
+    fn move_beams(&self, beam_positions: &HashMap<Point2D, usize>) -> HashMap<Point2D, usize> {
+        // move down by 1 all values that aren't on splitters
+        beam_positions
+            .iter()
+            .filter(|(key, _)| !self.splitters.contains(key))
+            .map(|(key, value)| (*key + Point2D { x: 0, y: 1 }, *value))
+            .collect::<HashMap<Point2D, usize>>()
+    }
+
+    fn split_beams(&self, beam_positions: &HashMap<Point2D, usize>) -> HashMap<Point2D, usize> {
+        // add left/right all values that are on splitters
+        beam_positions
+            .iter()
+            .filter(|(key, _)| self.splitters.contains(key))
+            .fold(beam_positions.clone(), |mut acc, (splitter_pos, value)| {
+                [
+                    *splitter_pos + Point2D { x: -1, y: 0 },
+                    *splitter_pos + Point2D { x: 1, y: 0 },
+                ]
+                .iter()
+                .for_each(|split_pos| *(acc.entry(*split_pos).or_insert(0)) += value);
+                acc
+            })
     }
 }
 
@@ -93,7 +98,7 @@ mod tests {
     #[test]
     fn test_calculate_day_b() {
         let day7 = Day7::new("data/test_data.txt").unwrap();
-        let expected = 0;
+        let expected = 40;
         let actual = day7.calculate_day_b();
         assert_eq!(expected, actual);
     }
@@ -109,7 +114,7 @@ mod tests {
     #[test]
     fn test_real_input_calculate_day_b() {
         let day7 = Day7::new("data/input_data.txt").unwrap();
-        let expected = 0;
+        let expected = 24743903847942;
         let actual = day7.calculate_day_b();
         assert_eq!(expected, actual);
     }
