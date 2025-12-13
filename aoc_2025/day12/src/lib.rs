ecb8c8609566c65569f490fee353e458315ddec8
Michael Walsh <triorph@gmail.com>
2025-12-13 14:57:52 +1300

    Day 12 - cheap solution but worked

--- aoc_2025/day12/src/lib.rs
+++ aoc_2025/day12/src/lib.rs
@@ -1,17 +1,19 @@
 mod parser;
-use crate::parser::parse_data;
+use std::collections::HashMap;
+
+use crate::parser::{parse_data, Goal, Present, PresentTile};
 use aoc_helpers::{read_input_file, AOCCalculator, AOCFileOrParseError};
 
 #[derive(Debug, Clone, PartialEq, Eq)]
 pub struct Day12 {
-    data: Vec<usize>,
+    presents: Vec<Present>,
+    goals: Vec<Goal>,
 }
 
 impl AOCCalculator for Day12 {
     fn new(filename: &str) -> Result<Day12, AOCFileOrParseError> {
-        Ok(Day12 {
-            data: parse_data(&read_input_file(filename)?)?,
-        })
+        let (presents, goals) = parse_data(&read_input_file(filename)?)?;
+        Ok(Day12 { presents, goals })
     }
 
     fn print_results(&self, name: &str) {
@@ -22,11 +24,39 @@ impl AOCCalculator for Day12 {
 
 impl Day12 {
     fn calculate_day_a(&self) -> usize {
-        0
+        self.goals
+            .iter()
+            .filter(|goal| !self.too_many(goal))
+            .count()
     }
 
     fn calculate_day_b(&self) -> usize {
         0
+    }
+
+    fn too_few(&self, goal: &Goal) -> bool {
+        let num_3x3_tiles = (goal.0 / 3) * (goal.1 / 3);
+        num_3x3_tiles > goal.2.iter().sum::<usize>()
+    }
+
+    fn too_many(&self, goal: &Goal) -> bool {
+        let count: HashMap<usize, usize> =
+            HashMap::from_iter(self.presents.iter().map(|(index, present)| {
+                (
+                    *index,
+                    present
+                        .iter()
+                        .flatten()
+                        .filter(|tile| **tile == PresentTile::Full)
+                        .count(),
+                )
+            }));
+        goal.2
+            .iter()
+            .enumerate()
+            .map(|(i, quantity)| count.get(&i).unwrap() * quantity)
+            .sum::<usize>()
+            > goal.0 * goal.1
     }
 }
 
@@ -38,7 +68,7 @@ mod tests {
     #[test]
     fn test_calculate_day_a() {
         let day12 = Day12::new("data/test_data.txt").unwrap();
-        let expected = 0;
+        let expected = 2;
         let actual = day12.calculate_day_a();
         assert_eq!(expected, actual);
     }
--- aoc_2025/day12/src/parser.rs
+++ aoc_2025/day12/src/parser.rs
@@ -1,16 +1,39 @@
 extern crate peg;
 use aoc_helpers::AOCFileOrParseError;
 
-pub enum PresentTile {}
+#[derive(PartialEq, Eq, Copy, Clone, Debug)]
+pub enum PresentTile {
+    Full,
+    Empty,
+}
+
+pub type Present = (usize, Vec<Vec<PresentTile>>);
+pub type Goal = (usize, usize, Vec<usize>);
 
 peg::parser! { pub grammar day12_parser() for str {
     rule number() -> usize
         = n:$(['0'..='9']+) { n.parse().unwrap_or_else(|_| panic!("Was expecting a number string {}", n))}
-    pub rule parse() -> Vec<usize>
-        = lines:number() ++ ("\n" +) "\n" * { lines }
+    rule full_present() -> PresentTile
+        = "#" { PresentTile::Full }
+    rule empty_present() -> PresentTile
+        = "." { PresentTile::Empty }
+    rule present_tile() -> PresentTile
+        = present_tile:(full_present() / empty_present()) { present_tile }
+    rule present_line() -> Vec<PresentTile>
+        = present_line:present_tile() ++ "" { present_line }
+    rule present() -> Present
+        = index:number() ":\n" present:present_line() ++ "\n" { (index, present) }
+    rule presents() -> Vec<Present>
+        = presents:present() ++ ("\n"+) { presents }
+    rule goal() -> Goal
+        = x:number() "x" y:number() ": " present_counts:number() ++ " " { (x, y, present_counts) }
+    rule goals() -> Vec<(Goal)>
+        = goals:goal() ++ "\n" { goals }
+    pub rule parse() -> (Vec<Present>, Vec<Goal>)
+        = presents:presents() "\n" * goals:goals() "\n"* { (presents, goals) }
 }}
 
-pub fn parse_data(input: &str) -> Result<Vec<usize>, AOCFileOrParseError> {
+pub fn parse_data(input: &str) -> Result<(Vec<Present>, Vec<Goal>), AOCFileOrParseError> {
     if let Ok(ret) = day12_parser::parse(input) {
         Ok(ret)
     } else {
@@ -27,8 +50,11 @@ mod test {
     #[test]
     fn test_parse() {
         let input_str = read_input_file("data/test_data.txt").unwrap();
-        let actual = day12_parser::parse(&input_str).expect("Should parse successfully");
-        let expected: Vec<usize> = vec![];
-        assert_eq!(expected, actual)
+        let (actual_presents, actual_goals) =
+            day12_parser::parse(&input_str).expect("Should parse successfully");
+        let expected_presents: Vec<Present> = vec![];
+        let expected_goals: Vec<Goal> = vec![];
+        assert_eq!(expected_presents, actual_presents);
+        assert_eq!(expected_goals, actual_goals);
     }
 }
